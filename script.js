a6f8091282ff48d2e76fca96b870dcd772f2a8bd
risi-prog <risitransport@gmail.com>
2026-06-01 21:48:45 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,40 @@
+const noBtn = document.getElementById("noBtn");
+
+noBtn.addEventListener("mouseover", () => {
+
+    const x = Math.random() * 300 - 150;
+    const y = Math.random() * 300 - 150;
+
+    noBtn.style.transform =
+    `translate(${x}px,${y}px)`;
+});
+
+function yesPage(){
+    document.getElementById("page1").classList.add("hidden");
+    document.getElementById("page2").classList.remove("hidden");
+}
+
+function datePage(){
+    document.getElementById("page2").classList.add("hidden");
+    document.getElementById("page3").classList.remove("hidden");
+}
+
+function foodPage(){
+    document.getElementById("page3").classList.add("hidden");
+    document.getElementById("page4").classList.remove("hidden");
+}
+
+function finish(food){
+
+    const date =
+    document.getElementById("date").value;
+
+    const time =
+    document.getElementById("time").value;
+
+    document.getElementById("page4").classList.add("hidden");
+    document.getElementById("page5").classList.remove("hidden");
+
+    document.getElementById("result").innerHTML =
+    `📅 ${date}<br>⏰ ${time}<br>🍴 ${food}`;
+}
