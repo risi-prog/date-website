3343c0212d4b7ee36c7f1a1937d8ae15a79c2fc2
risi-prog <risitransport@gmail.com>
2026-06-01 21:55:55 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,47 +1,44 @@
 const noBtn = document.getElementById("noBtn");
 
 noBtn.addEventListener("mouseover", () => {
-
     const x = Math.random() * 300 - 150;
     const y = Math.random() * 300 - 150;
 
-    noBtn.style.transform =
-    `translate(${x}px,${y}px)`;
+    noBtn.style.transform = `translate(${x}px, ${y}px)`;
 });
 
-function yesPage(){
+function yesPage() {
     document.getElementById("page1").classList.add("hidden");
     document.getElementById("page2").classList.remove("hidden");
 }
 
-function datePage(){
+function datePage() {
     document.getElementById("page2").classList.add("hidden");
     document.getElementById("page3").classList.remove("hidden");
 }
 
-function foodPage(){
+function foodPage() {
     document.getElementById("page3").classList.add("hidden");
     document.getElementById("page4").classList.remove("hidden");
 }
 
-function finish(food){
-
-    const date =
-    document.getElementById("date").value;
-
-    const time =
-    document.getElementById("time").value;
+function finish(food) {
+    const date = document.getElementById("date").value;
+    const time = document.getElementById("time").value;
 
     document.getElementById("page4").classList.add("hidden");
     document.getElementById("page5").classList.remove("hidden");
-document.getElementById("result").innerHTML =
-`
-📅 ${date}<br>
-⏰ ${time}<br>
-🍴 ${food}<br><br>
 
-💕 Date confirmed! 💕<br>
-I can't wait to see your smile 😊<br>
-See you soon ❤️
-`;
+    document.getElementById("result").innerHTML = `
+        <h2>💕 Date Confirmed! 💕</h2>
+        <p>📅 <strong>${date}</strong></p>
+        <p>⏰ <strong>${time}</strong></p>
+        <p>🍴 <strong>${food}</strong></p>
+
+        <br>
+
+        <p>😊I can't wait to see your smile!</p>
+        <p>❤️ See you soon ❤️</p>
+        
+    `;
 }
